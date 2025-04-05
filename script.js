b19b36f25c919f78e28d2cd209d173e60669ff82
codewjuan <125355229+setjuanidev@users.noreply.github.com>
2025-04-05 18:15:17 +0200

    Create script.js

---
+++ script.js
@@ -0,0 +1,15 @@
+[JavaScript - script.js]
+const sections = document.querySelectorAll('.section');
+const observer = new IntersectionObserver(entries => {
+  entries.forEach(entry => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add('visible');
+    }
+  });
+}, {
+  threshold: 0.1
+});
+
+sections.forEach(section => {
+  observer.observe(section);
+});
